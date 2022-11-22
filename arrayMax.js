e45b11d0f2736ebb2abf932a4432d3c5a0703837
jaceksen <jaceksen@gmail.com>
2022-11-22 23:34:39 +0100

    leftshift

---
+++ arrayMax.js
@@ -0,0 +1,3 @@
+const findLargestNum = (arr) => Math.max(...arr)
+
+console.log(findLargestNum([300, 200, 600, 150]));
---
+++ leftShift.js
@@ -0,0 +1,3 @@
+const shiftToLeft = (x, y) => x * 2**y
+
+console.log(shiftToLeft(5,2))
