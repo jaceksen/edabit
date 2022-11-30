933e4162bf622162f08c1a9b33354eabbaf2ad9b
jaceksen <jaceksen@gmail.com>
2022-11-30 01:07:41 +0100

    eval replace

---
+++ stringToDzialanie.js
@@ -0,0 +1,32 @@
+const isTrue = (relation) => {
+    // const pos = relation.includes('=') ? relation.indexOf('=')
+    //     : relation.includes('>') ? relation.indexOf('>')
+    //     : relation.includes('<') ? relation.indexOf('<') 
+    //     : null
+    // console.log('pos', pos);
+
+    // return relation.includes('=') ? (relation.split('=')[0]) === (relation.split('=')[1])
+    //     : relation.includes('>') ? (relation.split('>')[0]) > (relation.split('>')[1])
+    //         : relation.includes('<') ? (relation.split('<')[0]) < (relation.split('<')[1])
+    //             : false 
+    // console.log('char', char);
+    // tab = relation.split(char);
+    // console.log(tab);
+    
+    let wyn = ''
+    relation.includes('=') ? wyn = relation.split('=')[0] : false
+
+    console.log(wyn);
+
+    // let wyn = relation.includes('=') ? relation.split('=')[0] === relation.split('=')[1]
+    //     : relation.includes('>') ? relation.split('=')[0] > relation.split('=')[1]
+    //         : relation.includes('<') ? relation.split('=')[0] < relation.split('=')[1]
+    //             : false
+    // console.log('wyn', wyn);
+    // return wyn;
+}
+
+
+console.log(isTrue('15>4'));
+
+// console.log(eval('2=2'));
